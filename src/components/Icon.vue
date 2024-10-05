90235a8557f788b4e7e4fa6481227a6951ad8266
kaudaouda <konedaouda7894@yahoo.com>
2024-10-05 04:34:55 +0000

    added material design icon and option to edit and add a todo

---
+++ src/components/Icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    :viewBox="viewBox"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path :d="path" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    path: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: [String, Number],
+      default: 24,
+    },
+    viewBox: {
+      type: String,
+      default: '0 0 24 24',
+    },
+  },
+};
+</script>
