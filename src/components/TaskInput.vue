0235428fae0edd8a24adca12a59214d21ea8c16a
kaudaouda <konedaouda7894@yahoo.com>
2024-10-06 15:41:33 +0000

    create special component or add to do

---
+++ src/components/TaskInput.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+.flex.mb-6.space-x-2
+  input.flex-1.p-3.border.rounded-lg(
+    v-model='taskInput' 
+    type='text' 
+    :placeholder='placeholder' 
+    class='focus:outline-none focus:ring-2 focus:ring-orange-500'
+    @keyup.enter='handleAddTask'
+  )
+  button.bg-orange-500.text-white.px-6.py-3.rounded-lg.transition-colors.flex.items-center.justify-center.font-bold(
+    @click='handleAddTask'
+    class='hover:bg-orange-300'
+    :aria-label='buttonLabel'
+  )
+    icon(:path='mdiPlusCircleOutline' size='36' class='text-white')
+    | {{ buttonText }}
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { mdiPlusCircleOutline } from '@mdi/js';
+
+export default defineComponent({
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Ajouter une nouvelle tâche...'
+    },
+    buttonText: {
+      type: String,
+      default: 'Ajouter'
+    },
+    buttonLabel: {
+      type: String,
+      default: 'Ajouter une tâche'
+    }
+  },
+  emits: ['add-task'],
+  setup(props, { emit }) {
+    const taskInput = ref('');
+
+    const handleAddTask = () => {
+      if (taskInput.value.trim()) {
+        emit('add-task', taskInput.value.trim());
+        taskInput.value = '';
+      }
+    };
+
+    return {
+      taskInput,
+      handleAddTask,
+      mdiPlusCircleOutline
+    };
+  }
+});
+</script>
